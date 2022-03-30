b00a7f18b019da1bf32459ead20a370b5637b3b0
Adnan Ali <adnantech17@gmail.com>
2022-03-30 20:14:42 +0600

    init: Bootstrap backend and frontend

---
+++ frontend/src/pages/TableList/index.jsx
@@ -0,0 +1,333 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, message, Input, Drawer } from 'antd';
+import React, { useState, useRef } from 'react';
+import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
+import ProTable from '@ant-design/pro-table';
+import { ModalForm, ProFormText, ProFormTextArea } from '@ant-design/pro-form';
+import ProDescriptions from '@ant-design/pro-descriptions';
+import UpdateForm from './components/UpdateForm';
+import { rule, addRule, updateRule, removeRule } from '@/services/ant-design-pro/api';
+/**
+ * @en-US Add node
+ * @zh-CN 添加节点
+ * @param fields
+ */
+
+const handleAdd = async (fields) => {
+  const hide = message.loading('正在添加');
+
+  try {
+    await addRule({ ...fields });
+    hide();
+    message.success('Added successfully');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Adding failed, please try again!');
+    return false;
+  }
+};
+/**
+ * @en-US Update node
+ * @zh-CN 更新节点
+ *
+ * @param fields
+ */
+
+const handleUpdate = async (fields) => {
+  const hide = message.loading('Configuring');
+
+  try {
+    await updateRule({
+      name: fields.name,
+      desc: fields.desc,
+      key: fields.key,
+    });
+    hide();
+    message.success('Configuration is successful');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Configuration failed, please try again!');
+    return false;
+  }
+};
+/**
+ *  Delete node
+ * @zh-CN 删除节点
+ *
+ * @param selectedRows
+ */
+
+const handleRemove = async (selectedRows) => {
+  const hide = message.loading('正在删除');
+  if (!selectedRows) return true;
+
+  try {
+    await removeRule({
+      key: selectedRows.map((row) => row.key),
+    });
+    hide();
+    message.success('Deleted successfully and will refresh soon');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('Delete failed, please try again');
+    return false;
+  }
+};
+
+const TableList = () => {
+  /**
+   * @en-US Pop-up window of new window
+   * @zh-CN 新建窗口的弹窗
+   *  */
+  const [createModalVisible, handleModalVisible] = useState(false);
+  /**
+   * @en-US The pop-up window of the distribution update window
+   * @zh-CN 分布更新窗口的弹窗
+   * */
+
+  const [updateModalVisible, handleUpdateModalVisible] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
+  const actionRef = useRef();
+  const [currentRow, setCurrentRow] = useState();
+  const [selectedRowsState, setSelectedRows] = useState([]);
+  /**
+   * @en-US International configuration
+   * @zh-CN 国际化配置
+   * */
+
+  const columns = [
+    {
+      title: 'Rule Name',
+      dataIndex: 'name',
+      tip: 'The rule name is the unique key',
+      render: (dom, entity) => {
+        return (
+          <a
+            onClick={() => {
+              setCurrentRow(entity);
+              setShowDetail(true);
+            }}
+          >
+            {dom}
+          </a>
+        );
+      },
+    },
+    {
+      title: 'Description',
+      dataIndex: 'desc',
+      valueType: 'textarea',
+    },
+    {
+      title: 'Number of Service Calls',
+      dataIndex: 'callNo',
+      sorter: true,
+      hideInForm: true,
+      renderText: (val) => `${val}${'0000'}`,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      hideInForm: true,
+      valueEnum: {
+        0: {
+          text: 'default',
+          status: 'Default',
+        },
+        1: {
+          text: 'running',
+          status: 'Processing',
+        },
+        2: {
+          text: 'online',
+          status: 'Success',
+        },
+        3: {
+          text: 'abnormal',
+          status: 'Error',
+        },
+      },
+    },
+    {
+      title: 'Last Scheduled at',
+      sorter: true,
+      dataIndex: 'updatedAt',
+      valueType: 'dateTime',
+      renderFormItem: (item, { defaultRender, ...rest }, form) => {
+        const status = form.getFieldValue('status');
+
+        if (`${status}` === '0') {
+          return false;
+        }
+
+        if (`${status}` === '3') {
+          return <Input {...rest} placeholder={'Please enter the reason for the exception!'} />;
+        }
+
+        return defaultRender(item);
+      },
+    },
+    {
+      title: 'Option',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => [
+        <a
+          key="config"
+          onClick={() => {
+            handleUpdateModalVisible(true);
+            setCurrentRow(record);
+          }}
+        >
+          Configuration
+        </a>,
+        <a key="subscribeAlert" href="https://procomponents.ant.design/">
+          Subscribe to alerts
+        </a>,
+      ],
+    },
+  ];
+  return (
+    <PageContainer>
+      <ProTable
+        headerTitle={'Enquiry Form'}
+        actionRef={actionRef}
+        rowKey="key"
+        search={{
+          labelWidth: 120,
+        }}
+        toolBarRender={() => [
+          <Button
+            type="primary"
+            key="primary"
+            onClick={() => {
+              handleModalVisible(true);
+            }}
+          >
+            <PlusOutlined /> New
+          </Button>,
+        ]}
+        request={rule}
+        columns={columns}
+        rowSelection={{
+          onChange: (_, selectedRows) => {
+            setSelectedRows(selectedRows);
+          },
+        }}
+      />
+      {selectedRowsState?.length > 0 && (
+        <FooterToolbar
+          extra={
+            <div>
+              chosen{' '}
+              <a
+                style={{
+                  fontWeight: 600,
+                }}
+              >
+                {selectedRowsState.length}
+              </a>{' '}
+              item &nbsp;&nbsp;
+              <span>
+                Total Number of Service Calls{' '}
+                {selectedRowsState.reduce((pre, item) => pre + item.callNo, 0)} 0000
+              </span>
+            </div>
+          }
+        >
+          <Button
+            onClick={async () => {
+              await handleRemove(selectedRowsState);
+              setSelectedRows([]);
+              actionRef.current?.reloadAndRest?.();
+            }}
+          >
+            bacth deletion
+          </Button>
+          <Button type="primary">batch approval</Button>
+        </FooterToolbar>
+      )}
+      <ModalForm
+        title={'New Rule'}
+        width="400px"
+        visible={createModalVisible}
+        onVisibleChange={handleModalVisible}
+        onFinish={async (value) => {
+          const success = await handleAdd(value);
+
+          if (success) {
+            handleModalVisible(false);
+
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+      >
+        <ProFormText
+          rules={[
+            {
+              required: true,
+              message: 'Rule name is required',
+            },
+          ]}
+          width="md"
+          name="name"
+        />
+        <ProFormTextArea width="md" name="desc" />
+      </ModalForm>
+      <UpdateForm
+        onSubmit={async (value) => {
+          const success = await handleUpdate(value);
+
+          if (success) {
+            handleUpdateModalVisible(false);
+            setCurrentRow(undefined);
+
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+        onCancel={() => {
+          handleUpdateModalVisible(false);
+
+          if (!showDetail) {
+            setCurrentRow(undefined);
+          }
+        }}
+        updateModalVisible={updateModalVisible}
+        values={currentRow || {}}
+      />
+
+      <Drawer
+        width={600}
+        visible={showDetail}
+        onClose={() => {
+          setCurrentRow(undefined);
+          setShowDetail(false);
+        }}
+        closable={false}
+      >
+        {currentRow?.name && (
+          <ProDescriptions
+            column={2}
+            title={currentRow?.name}
+            request={async () => ({
+              data: currentRow || {},
+            })}
+            params={{
+              id: currentRow?.name,
+            }}
+            columns={columns}
+          />
+        )}
+      </Drawer>
+    </PageContainer>
+  );
+};
+
+export default TableList;
