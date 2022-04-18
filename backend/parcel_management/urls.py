5621ceef4d5c583ea43f9ec4610a861f709e17ce
Mohidul Haque Mridul <mridul.haque.mh@gmail.com>
2022-04-18 22:57:13 +0600

    test

--- backend/parcel_management/urls.py
+++ backend/parcel_management/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import ParcelAPI, CheckReceiveAPI, CheckRouteAPI, TrackAPI
 
 urlpatterns = [
-    path('parcel/', ParcelAPI.as_view()),
-    path('parcel/<int:pk>/', ParcelAPI.as_view()),
-    path('check-receive/<int:pk>/', CheckReceiveAPI.as_view()),
-    path('check-route/<int:pk>/', CheckRouteAPI.as_view()),
-    path('receive/', CheckReceiveAPI.as_view()),
-    path('route/', CheckRouteAPI.as_view()),
-    path('track/', TrackAPI.as_view()),
+    path('parcel/', ParcelAPI.as_view(), name = 'parcel_api'),
+    path('parcel/<int:pk>/', ParcelAPI.as_view(), name = 'parcel_api_with_primary_key'),
+    path('check-receive/<int:pk>/', CheckReceiveAPI.as_view(), name = 'check_receive_api_with_primary_key'),
+    path('check-route/<int:pk>/', CheckRouteAPI.as_view(), name = 'check_route_api_with_primary_key'),
+    path('receive/', CheckReceiveAPI.as_view(), name='check_receive_api'),
+    path('route/', CheckRouteAPI.as_view(), name = 'check_route_api'),
+    path('track/', TrackAPI.as_view(), name = 'track_api'),
 ]
